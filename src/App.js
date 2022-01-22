4874d50509dd13cae949a9a665f6142047e27035
suneelfm <sfm.20031998@gmail.com>
2022-01-22 18:11:56 +0530

    Fixed navigation issue of setting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Calculator from './pages/Calculator/Calculator';
-import HealthRecords from './pages/HealthRecords/HealthRecords';
-import Home from './pages/Home/Home';
-import Layout from './pages/Layout/Layout';
-import OfficialRecords from './pages/OfficialRecords/OfficialRecords';
-import OtherRecords from './pages/OtherRecords/OtherRecords';
-import PersonalRecords from './pages/PersonalRecors/PersonalRecords';
-import ToDoList from './pages/ToDoList/ToDoList';
-import { getPersonalRecords } from './Redux/Actions/getPersonalRecords';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Calculator from "./pages/Calculator/Calculator";
+import HealthRecords from "./pages/HealthRecords/HealthRecords";
+import Home from "./pages/Home/Home";
+import Layout from "./pages/Layout/Layout";
+import OfficialRecords from "./pages/OfficialRecords/OfficialRecords";
+import OtherRecords from "./pages/OtherRecords/OtherRecords";
+import PersonalRecords from "./pages/PersonalRecors/PersonalRecords";
+import Settings from "./pages/Settings/Settings";
+import ToDoList from "./pages/ToDoList/ToDoList";
+import { getPersonalRecords } from "./Redux/Actions/getPersonalRecords";
 
 function App() {
   const dispatch = useDispatch();
@@ -22,14 +23,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>} />
-          <Route path='todolist' element={<ToDoList/>} />
-          <Route path='calculator' element={<Calculator/>} />
-          <Route path='details/personalrecords' element={<PersonalRecords/>} />
-          <Route path='details/officialrecords' element={<OfficialRecords/>} />
-          <Route path='details/healthrecords' element={<HealthRecords/>} />
-          <Route path='otherrecords' element={<OtherRecords/>} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="todolist" element={<ToDoList />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="details/personalrecords" element={<PersonalRecords />} />
+          <Route path="details/officialrecords" element={<OfficialRecords />} />
+          <Route path="details/healthrecords" element={<HealthRecords />} />
+          <Route path="otherrecords" element={<OtherRecords />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
     </BrowserRouter>
