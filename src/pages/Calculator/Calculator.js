700d4541e6633501f5dff13a375838baa26cf3b5
suneelfm <sfm.20031998@gmail.com>
2022-01-24 13:24:11 +0530

    Changed the size of calcy

--- src/pages/Calculator/Calculator.js
+++ src/pages/Calculator/Calculator.js
@@ -122,212 +122,220 @@ export default function Calculator() {
           </div>
         </div>
       </div>
-      <div className="row" style={{ marginTop: "1vw" }}>
-        <table className="tableframe">
-          <thead>
-            <tr>
-              <td colSpan={4}>
-                <div className="calcyDisplay">
-                  <div
-                    className="typeText"
-                    style={{
-                      display: disppadding ? "none" : "flex",
-                      padding: disppadding ? "0px" : "1vw 2vw",
-                    }}
-                  >
-                    {display}
+      <div
+        className="row"
+        style={{ marginTop: "1vw", display: "flex", justifyContent: "center" }}
+      >
+        <div className="tableframe">
+          <table style={{width:"100%"}}>
+            <thead>
+              <tr>
+                <td colSpan={4}>
+                  <div className="calcyDisplay">
+                    <div
+                      className="typeText"
+                      style={{
+                        display: disppadding ? "none" : "flex",
+                        padding: disppadding ? "0px" : "1vw 2vw",
+                      }}
+                    >
+                      {display}
+                    </div>
+                    <div
+                      className="result"
+                      style={{
+                        color: disppadding ? "black" : "rgb(173, 173, 173)",
+                        fontSize: disppadding ? "5vw" : "2vw",
+                      }}
+                    >
+                      {result}
+                    </div>
                   </div>
-                  <div
-                    className="result"
-                    style={{
-                      color: disppadding ? "black" : "rgb(173, 173, 173)",
-                      fontSize: disppadding ? "5vw" : "2vw",
-                    }}
+                </td>
+              </tr>
+            </thead>
+            <tbody style={{ padding: "1vw" }}>
+              <tr>
+                <td style={{ width: "25%", padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={clearDisplay}
                   >
-                    {result}
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td style={{ width: "25%" }}>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={clearDisplay}
-                >
-                  C
-                </button>
-              </td>
-              <td style={{ width: "25%" }}>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen("(")}
-                  style={{ width: "35%" }}
-                >
-                  (
-                </button>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(")")}
-                  style={{ width: "35%", float: "right", marginRight: "4.5vw" }}
-                >
-                  )
-                </button>
-              </td>
-              <td style={{ width: "25%" }}>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen("\u0025")}
-                >
-                  <i className="fas fa-percentage"></i>
-                </button>
-              </td>
-              <td className="operatorbutton">
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={backSpace}
-                >
-                  <i className="fas fa-backspace" />
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(7)}
-                >
-                  7
-                </button>
-              </td>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(8)}
-                >
-                  8
-                </button>
-              </td>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(9)}
-                >
-                  9
-                </button>
-              </td>
-              <td className="operatorbutton">
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen("+")}
-                >
-                  <i className="fas fa-plus"></i>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(4)}
-                >
-                  4
-                </button>
-              </td>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(5)}
-                >
-                  5
-                </button>
-              </td>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(6)}
-                >
-                  6
-                </button>
-              </td>
-              <td className="operatorbutton">
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen("-")}
-                >
-                  <i className="fas fa-minus"></i>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(1)}
-                >
-                  1
-                </button>
-              </td>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(2)}
-                >
-                  2
-                </button>
-              </td>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(3)}
-                >
-                  3
-                </button>
-              </td>
-              <td className="operatorbutton">
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen("\u00D7")}
-                >
-                  <i className="fas fa-times"></i>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(".")}
-                >
-                  .
-                </button>
-              </td>
-              <td>
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen(0)}
-                >
-                  0
-                </button>
-              </td>
-              <td>
-                <button className="fitToCell equalbutton" onClick={calculation}>
-                  =
-                </button>
-              </td>
-              <td className="operatorbutton">
-                <button
-                  className="fitToCell calcybuttonProp"
-                  onClick={() => calcyScreen("\u00F7")}
-                >
-                  <i className="fas fa-divide"></i>
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                    C
+                  </button>
+                </td>
+                <td style={{ width: "25%", padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen("(")}
+                    style={{ width: "35%" }}
+                  >
+                    (
+                  </button>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(")")}
+                    style={{ width: "35%", marginLeft: "1vw" }}
+                  >
+                    )
+                  </button>
+                </td>
+                <td style={{ width: "25%", padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen("\u0025")}
+                  >
+                    <i className="fas fa-percentage"></i>
+                  </button>
+                </td>
+                <td className="operatorbutton">
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={backSpace}
+                  >
+                    <i className="fas fa-backspace" />
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(7)}
+                  >
+                    7
+                  </button>
+                </td>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(8)}
+                  >
+                    8
+                  </button>
+                </td>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(9)}
+                  >
+                    9
+                  </button>
+                </td>
+                <td className="operatorbutton" style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen("+")}
+                  >
+                    <i className="fas fa-plus"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(4)}
+                  >
+                    4
+                  </button>
+                </td>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(5)}
+                  >
+                    5
+                  </button>
+                </td>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(6)}
+                  >
+                    6
+                  </button>
+                </td>
+                <td className="operatorbutton" style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen("-")}
+                  >
+                    <i className="fas fa-minus"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(1)}
+                  >
+                    1
+                  </button>
+                </td>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(2)}
+                  >
+                    2
+                  </button>
+                </td>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(3)}
+                  >
+                    3
+                  </button>
+                </td>
+                <td className="operatorbutton" style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen("\u00D7")}
+                  >
+                    <i className="fas fa-times"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(".")}
+                  >
+                    .
+                  </button>
+                </td>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen(0)}
+                  >
+                    0
+                  </button>
+                </td>
+                <td style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell equalbutton"
+                    onClick={calculation}
+                  >
+                    =
+                  </button>
+                </td>
+                <td className="operatorbutton" style={{ padding: "1vw" }}>
+                  <button
+                    className="fitToCell calcybuttonProp"
+                    onClick={() => calcyScreen("\u00F7")}
+                  >
+                    <i className="fas fa-divide"></i>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
