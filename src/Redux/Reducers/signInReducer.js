a4181c47c4d43f7c55b1fcba3fac5ac0c106cf31
suneelfm <sfm.20031998@gmail.com>
2022-01-31 19:14:21 +0530

    Fixed profile photo issue in server

--- src/Redux/Reducers/signInReducer.js
+++ src/Redux/Reducers/signInReducer.js
@@ -5,7 +5,7 @@ const credentialList = {
       mailid: "sfm.20031998@gmail.com",
       password: "123",
       name: "Suneel F M",
-      img: { name: "/Images/user.jpg" },
+      img: { name: "/Images/user.JPG" },
     },
   ],
   loggedInUser: {},
